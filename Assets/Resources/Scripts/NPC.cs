b1577cc766760a29851a6015075cfd5369f27a8d
Kevin Van Cleave <kevin.vancleave@wustl.edu>
2020-02-07 13:25:22 -0600

    first attempt at NPC following (kevin)

---
+++ Assets/Resources/Scripts/NPC.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+
+    SpriteRenderer sr;
+    bool enemyInSight;
+    List<GameObject> enemies;
+
+    public Sprite redPlayer;
+    public Sprite bluePlayer;
+    public Sprite greenPlayer;
+    public Sprite yellowPlayer;
+    public Sprite redNPC;
+    public Sprite blueNPC;
+    public Sprite greenNPC;
+    public Sprite yellowNPC;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        enemyInSight = false;
+        enemies = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyInSight)
+        {
+            //find closest enemy
+
+            //first find this position
+            Vector3 thisPos = transform.position;
+
+            //go through all enemies
+            float shortestDistance = 1000f;
+            Vector3 closestEnemyPosition = transform.position;
+            foreach(GameObject enemy in enemies)
+            {
+                float calculatedDistance = Vector3.Distance(thisPos, enemy.transform.position);
+                if(calculatedDistance <= shortestDistance)
+                {
+                    shortestDistance = calculatedDistance;
+                    closestEnemyPosition = enemy.transform.position;
+                }
+            }
+
+
+            //move towards that enemy
+            transform.position = Vector3.MoveTowards(thisPos, closestEnemyPosition, speed);
+            //print(closestEnemyPosition.ToString());
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //print("enter trigger");
+        SpriteRenderer otherSR = collision.gameObject.GetComponent<SpriteRenderer>();
+        Sprite otherTeam = otherSR.sprite;
+        Sprite thisTeam = sr.sprite;
+
+        if(thisTeam == redNPC && !(otherTeam == redNPC || otherTeam == redPlayer))
+        {
+            enemyInSight = true;
+            if (!enemies.Contains(collision.gameObject))
+            {
+                enemies.Add(collision.gameObject);
+            }
+        }
+        if (thisTeam == blueNPC && !(otherTeam == blueNPC || otherTeam == bluePlayer))
+        {
+            enemyInSight = true;
+            if (!enemies.Contains(collision.gameObject))
+            {
+                enemies.Add(collision.gameObject);
+            }
+        }
+        if (thisTeam == greenNPC && !(otherTeam == greenNPC || otherTeam == greenPlayer))
+        {
+            enemyInSight = true;
+            if (!enemies.Contains(collision.gameObject))
+            {
+                enemies.Add(collision.gameObject);
+            }
+        }
+        if (thisTeam == yellowNPC && !(otherTeam == yellowNPC || otherTeam == yellowPlayer))
+        {
+            enemyInSight = true;
+            if (!enemies.Contains(collision.gameObject))
+            {
+                enemies.Add(collision.gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //print("trigger exit");
+        enemies.Remove(collision.gameObject);
+        if(enemies.Count == 0)
+        {
+            enemyInSight = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Sprite thisTeam = sr.sprite;
+        Sprite otherTeam = collision.gameObject.GetComponent<SpriteRenderer>().sprite;
+
+        if (thisTeam == redNPC && !(otherTeam == redNPC || otherTeam == redPlayer))
+        {
+            Destroy(gameObject);   
+        }
+        if (thisTeam == blueNPC && !(otherTeam == blueNPC || otherTeam == bluePlayer))
+        {
+            Destroy(gameObject);
+        }
+        if (thisTeam == greenNPC && !(otherTeam == greenNPC || otherTeam == greenPlayer))
+        {
+            Destroy(gameObject);
+        }
+        if (thisTeam == yellowNPC && !(otherTeam == yellowNPC || otherTeam == yellowPlayer))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D _rb;
+    SpriteRenderer sr;
+
+    public Sprite redPlayer;
+    public Sprite bluePlayer;
+    public Sprite greenPlayer;
+    public Sprite yellowPlayer;
+    public Sprite redNPC;
+    public Sprite blueNPC;
+    public Sprite greenNPC;
+    public Sprite yellowNPC;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        if(sr.sprite == null)
+        {
+            sr.sprite = redPlayer;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        //directional movement with a-s-d-w
+        if (Input.GetKey(KeyCode.A))
+        {
+            _rb.AddForce(Vector2.left * 12f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _rb.AddForce(Vector2.right * 12f);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            _rb.AddForce(Vector2.up * 12f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _rb.AddForce(Vector2.down * 12f);
+        }
+
+
+
+
+        //change player sprites to fit colors
+        //can take out some colors or add refresh time
+        if (Input.GetKey(KeyCode.U) && sr.sprite != redPlayer)
+        {
+            sr.sprite = redPlayer;
+        }
+        if (Input.GetKey(KeyCode.I) && sr.sprite != bluePlayer)
+        {
+            sr.sprite = bluePlayer;
+        }
+        if (Input.GetKey(KeyCode.O) && sr.sprite != greenPlayer)
+        {
+            sr.sprite = greenPlayer;
+        }
+        if (Input.GetKey(KeyCode.P) && sr.sprite != yellowPlayer)
+        {
+            sr.sprite = yellowPlayer;
+        }
+
+
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //collision with another NPC
+        //check color to see if player reset
+
+        SpriteRenderer otherSR = collision.gameObject.GetComponent<SpriteRenderer>();
+        Sprite otherTeam = otherSR.sprite;
+        Sprite playerTeam = sr.sprite;
+
+        if(otherTeam == redNPC && playerTeam != redPlayer)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        if (otherTeam == blueNPC && playerTeam != bluePlayer)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        if (otherTeam == greenNPC && playerTeam != greenPlayer)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        if (otherTeam == yellowNPC && playerTeam != yellowPlayer)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
