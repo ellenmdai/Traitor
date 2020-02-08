d614bf40a29b06605f96ce4f9905256604aefd89
Ellen <12478985+ellenmdai@users.noreply.github.com>
2020-02-08 14:00:38 -0600

    beginning procedural POC using Q5 - created spawn point

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public static GameController instance;  // singleton
+    // ref to player cuz scrolling dependent on player's location on screen
+    public GameObject player; 
+    public Transform bgSpawnPoint;
+    public GameObject bgTilePrefab;
+    public GameObject[] bgTiles;   // bg tiles to produce
+
+    public float timeElapsed; // state tracking
+    void Awake() {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+    }
+
+    void SpawnBgTile() {
+        Instantiate(bgTilePrefab, bgSpawnPoint.position, Quaternion.identity);
+    }
+}
