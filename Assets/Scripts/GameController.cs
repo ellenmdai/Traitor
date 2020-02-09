0250bdd7f53348cbcd2b30e21edc6d861cb72c85
Ellen <12478985+ellenmdai@users.noreply.github.com>
2020-02-09 10:55:13 -0600

    using right arrow, background will scroll using randomly chosen tiles (rn between 3 samples). Tiles created at spawn point, destroyed at destroy point. GameController script attached to GameController object. Scrolling is slightly gitchy rn in that there are tiny gaps between tiles.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,26 +9,44 @@ public class GameController : MonoBehaviour
     // ref to player cuz scrolling dependent on player's location on screen
     public GameObject player; 
     public Transform bgSpawnPoint;
-    public GameObject bgTilePrefab;
-    public GameObject[] bgTiles;   // bg tiles to produce
+    public GameObject[] bgTilePrefabs;  // bg tiles to produce
+  
+    public float scrollSpeed;
+    public Transform destroyPoint;  // where off-screen tiles should be destroyed to free up memory
 
-    public float timeElapsed; // state tracking
+    private int numTilesSpawned;    // for logic and scorekeeping
+    private float progressSinceLastSpawn; // to track when to spawn new tile
+    private float tileWidth;
     void Awake() {
         instance = this;
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        numTilesSpawned = 0;
+        progressSinceLastSpawn = 0;
+        tileWidth = 2*bgTilePrefabs[0].GetComponent<SpriteRenderer>().bounds.extents.x;
+        Debug.Log("tileWidth: " + tileWidth);
     }
 
     // Update is called once per frame
     void Update()
     {
-        timeElapsed += Time.deltaTime;
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            progressSinceLastSpawn += scrollSpeed;
+        }
+        Debug.Log(progressSinceLastSpawn % tileWidth);
+        if (progressSinceLastSpawn >= tileWidth) {      // SLIGHTLY GLITCHY--small gaps btwn tiles
+            SpawnBgTile();
+        }
     }
 
     void SpawnBgTile() {
-        Instantiate(bgTilePrefab, bgSpawnPoint.position, Quaternion.identity);
+        progressSinceLastSpawn = 0;
+        GameObject randomBgTilePrefab = bgTilePrefabs[Random.Range(0, bgTilePrefabs.Length)];
+        GameObject newTile = Instantiate(randomBgTilePrefab, bgSpawnPoint.position, Quaternion.identity);
+        newTile.GetComponent<bgTileController>().destroyPoint = destroyPoint;
+        newTile.GetComponent<bgTileController>().scrollSpeed = scrollSpeed;
+        numTilesSpawned++;
     }
 }
---
+++ Assets/Scripts/bgTileController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bgTileController : MonoBehaviour
+{
+    public float scrollSpeed;   // rate at which bg passes; set by GameController
+    public Transform destroyPoint;  // to free up memory
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.position -= new Vector3(scrollSpeed, 0, 0);
+        }
+        if (transform.position.x <= destroyPoint.position.x) {
+            Destroy(gameObject);
+        }
+    }
+}
