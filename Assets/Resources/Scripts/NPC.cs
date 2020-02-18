2a5eba5636a92f50192033ff9187d7ef9da4844b
Kevin Van Cleave <kevin.vancleave@wustl.edu>
2020-02-18 17:15:22 -0600

    NPC routing

--- Assets/Resources/Scripts/NPC.cs
+++ Assets/Resources/Scripts/NPC.cs
@@ -16,11 +16,25 @@ public class NPC : MonoBehaviour
     public Transform viewPivot;
 
 
+    //path
+    public bool hasPathToFollow;
+    [SerializeField]
+    public Transform[] routes;
+    private int routeToGo;
+    private float tParam;
+    private Vector3 NPCPositionOnPath;
+    private bool coroutineAllowed;
+
     // Start is called before the first frame update
     void Start()
     {
         enemyInSight = false;
         enemies = new List<GameObject>();
+
+        //path
+        routeToGo = 0;
+        tParam = 0f;
+        coroutineAllowed = true;
     }
 
     // Update is called once per frame
@@ -28,33 +42,91 @@ public class NPC : MonoBehaviour
     {
         if (enemyInSight)
         {
-            //find closest enemy
+            ////find closest enemy
 
-            //first find this position
-            Vector3 thisPos = transform.position;
+            ////first find this position
+            //Vector3 thisPos = transform.position;
 
-            //go through all enemies
-            float shortestDistance = 1000f;
-            foreach(GameObject enemy in enemies)
-            {
-                float calculatedDistance = Vector3.Distance(thisPos, enemy.transform.position);
-                if(calculatedDistance <= shortestDistance)
-                {
-                    shortestDistance = calculatedDistance;
-                    closestEnemy = enemy;
-                }
-            }
+            ////go through all enemies
+            //float shortestDistance = 1000f;
+            //foreach(GameObject enemy in enemies)
+            //{
+            //    float calculatedDistance = Vector3.Distance(thisPos, enemy.transform.position);
+            //    if(calculatedDistance <= shortestDistance)
+            //    {
+            //        shortestDistance = calculatedDistance;
+            //        closestEnemy = enemy;
+            //    }
+            //}
 
 
-            //move towards that enemy
-            transform.position = Vector3.MoveTowards(thisPos, closestEnemy.transform.position, speed);
+            ////move towards that enemy
+            //transform.position = Vector3.MoveTowards(thisPos, closestEnemy.transform.position, speed);
 
-            Vector3 directionTowardsEnemy = transform.position - closestEnemy.transform.position;
-            float degreeAngleToEnemy = Mathf.Atan2(directionTowardsEnemy.y, directionTowardsEnemy.x) * 180f / Mathf.PI;
+            //Vector3 directionTowardsEnemy = transform.position - closestEnemy.transform.position;
+            //float degreeAngleToEnemy = Mathf.Atan2(directionTowardsEnemy.y, directionTowardsEnemy.x) * 180f / Mathf.PI;
 
-            viewPivot.rotation = Quaternion.Euler(0,0,degreeAngleToEnemy);
+            //viewPivot.rotation = Quaternion.Euler(0,0,degreeAngleToEnemy);
             
         }
+        else if(hasPathToFollow)
+        {
+            //follow path
+            if (coroutineAllowed)
+            {
+                StartCoroutine(GoByTheRoute(routeToGo));
+            }
+        }
+    }
+
+    private void rotateViewBasedOnVelocity(Vector3 velocity)
+    {
+        float degreeAngleToDirection = Mathf.Atan2(velocity.y, velocity.x) * 180f / Mathf.PI;
+
+        viewPivot.rotation = Quaternion.Euler(0, 0, degreeAngleToDirection-180f);
+    }
+
+    private IEnumerator GoByTheRoute(int routeNumber)
+    {
+        coroutineAllowed = false;
+         
+        Vector3 p0 = routes[routeNumber].GetChild(0).position;
+        Vector3 p1 = routes[routeNumber].GetChild(1).position;
+        Vector3 p2 = routes[routeNumber].GetChild(2).position;
+        Vector3 p3 = routes[routeNumber].GetChild(3).position;
+
+        Vector3 velocity = transform.position;
+
+        while (tParam < 1 )
+        {
+            tParam += Time.deltaTime * speed;
+
+            NPCPositionOnPath = Mathf.Pow(1 - tParam, 3) * p0 +
+                3 * tParam * Mathf.Pow(1 - tParam, 2) * p1 +
+                3 * (1 - tParam) * Mathf.Pow(tParam, 2) * p2 +
+                Mathf.Pow(tParam, 3) * p3;
+
+            velocity = transform.position - NPCPositionOnPath;
+
+            rotateViewBasedOnVelocity(velocity);
+
+            transform.position = NPCPositionOnPath;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+
+        tParam = 0f;
+
+        routeToGo += 1;
+
+        if(routeToGo > routes.Length - 1)
+        {
+            routeToGo = 0;
+        }
+
+        coroutineAllowed = true;
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
