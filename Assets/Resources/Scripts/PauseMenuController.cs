795b2c7795e7bd4243da2a491f6dbf2b69af3508
Ellen <12478985+ellenmdai@users.noreply.github.com>
2020-03-09 17:51:04 -0500

    Pause menu pauses game, resume button resumes game

---
+++ Assets/Resources/Scripts/PauseMenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public static PauseMenuController instance;
+    GameObject mainMenuButton;
+    GameObject instructionsButton;
+
+    void Awake() {
+        instance = this;
+    }
+
+    public void onResumeClick() {
+        Debug.Log("destroy");
+        Time.timeScale = 1f;
+        Destroy(gameObject);
+    }
+
+
+}
