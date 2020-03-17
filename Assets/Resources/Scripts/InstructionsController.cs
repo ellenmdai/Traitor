1dde754090adedcb56e2b002631a4fbc35910ba8
Ellen <12478985+ellenmdai@users.noreply.github.com>
2020-03-17 13:28:31 -0500

    Basic pause menu prefabs done; some UI components waiting upon further gameplay development. Glitch in that the overlay bar is not visiblue in the actual game despite being there in the prefab. TODO: fix glitch and main meu

---
+++ Assets/Resources/Scripts/InstructionsController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstructionsController : MonoBehaviour
+{
+
+    public static InstructionsController instance;
+    public GameObject[] pages;
+    public GameObject nextPageButton;
+    public GameObject prevPageButton;
+    public GameObject backButton;
+
+    // State
+    GameObject currPage;
+    int currPageNo;
+    
+    void Awake() {
+        instance = this;
+    }
+    void Start()
+    {
+        currPageNo = 0;
+        refreshPage();
+        // currPage = Instantiate(pages[currPageNo], Vector3.zero, Quaternion.identity);
+        // currPage.transform.SetParent(gameObject.GetComponent<Transform>());
+        // prevPageButton.GetComponent<GameObject>().SetActive(false);
+
+    }
+
+    public void onNextClick() {
+        ++currPageNo;
+        refreshPage();
+    }
+
+    public void onPrevClick() {
+        --currPageNo;
+        refreshPage();
+    }
+
+    public void onBackClick() {
+        Debug.Log("Close instructions");
+        Destroy(gameObject);
+    }
+
+    void refreshPage() {
+        Destroy(currPage);
+        currPage = Instantiate(pages[currPageNo], Vector3.zero, Quaternion.identity);
+        currPage.transform.SetParent(gameObject.GetComponent<Transform>());
+        nextPageButton.SetActive(currPageNo < pages.Length - 1);
+        prevPageButton.SetActive(currPageNo > 0);
+        // Debug.Log("NextPageButton active: " + nextPageButton.activeSelf);
+        // Debug.Log("PrevPageButton active: " + prevPageButton.activeSelf);
+
+    }
+
+}
--- Assets/Resources/Scripts/PauseMenuController.cs
+++ Assets/Resources/Scripts/PauseMenuController.cs
@@ -8,6 +8,8 @@ public class PauseMenuController : MonoBehaviour
     GameObject mainMenuButton;
     GameObject instructionsButton;
 
+    public GameObject instructionsMenu;
+
     void Awake() {
         instance = this;
     }
@@ -16,6 +18,11 @@ public class PauseMenuController : MonoBehaviour
         Debug.Log("destroy");
         Time.timeScale = 1f;
         Destroy(gameObject);
+    }
+
+    public void onInstructionsClick() {
+        Debug.Log("open instructions");
+        Instantiate(instructionsMenu, Vector3.zero, Quaternion.identity);
     }
 
 
